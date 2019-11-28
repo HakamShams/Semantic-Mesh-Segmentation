cac766113f41caab5866a87c200b6a7ce28c5776
HakamShams <49558174+HakamShams@users.noreply.github.com>
2019-11-28 02:35:44 +0100

    Add files via upload

---
+++ tf_ops/3d_interpolation/tf_interpolate_compile.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+/usr/local/cuda-9.0/bin/nvcc tf_interpolate_g.cu -o tf_interpolate_g.cu.o -c -O2 -DGOOGLE_CUDA=1 -x cu -Xcompiler -fPIC
+
+# TF1.2
+#g++ -std=c++11 tf_interpolate.cpp -o tf_interpolate_so.so -shared -fPIC -I /usr/local/lib/python2.7/dist-packages/tensorflow/include -I /usr/local/cuda-8.0/include -lcudart -L /usr/local/cuda-8.0/lib64/ -O2 -D_GLIBCXX_USE_CXX11_ABI=0
+
+# TF1.4
+#g++ -std=c++11 tf_interpolate.cpp -o tf_interpolate_so.so -shared -fPIC -I TF_INC -I /usr/local/cuda-9.0/include -I $TF_INC/external/nsync/public -lcudart -L /usr/local/cuda-8.0/lib64/ -L$TF_LIB/ -ltensorflow_framework -O2 -D_GLIBCXX_USE_CXX11_ABI=0
+
+# TF1.9
+conda activate (your conda env)
+TF_INC=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_include())')
+TF_LIB=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_lib())')
+
+g++ -std=c++11 tf_interpolate.cpp -o tf_interpolate_so.so -shared -fPIC -I $TF_INC -I /usr/local/cuda-9.0/include -I $TF_INC/external/nsync/public -lcudart -L /usr/local/cuda-9.0/lib64/ -L$TF_LIB/ -ltensorflow_framework -O2
